373d0b1a2ec7d033d9d9ed726b8a5eb60bb5e152
000SMS000 <mahmoud.m.m.ahmed2017@gmail.com>
2026-02-16 11:21:44 +0200

    wep page

--- src/server.js
+++ src/server.js
@@ -5,6 +5,9 @@ require("dotenv").config();
 const app = express();
 const PORT = 8080;
 
+app.use(express.json());
+app.use(express.static("public"));
+
 app.get("/weather", async (req, res) => {
   const city = req.query.city;
 
